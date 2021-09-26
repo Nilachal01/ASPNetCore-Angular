7d378be5d60316cd8535d616a493ff8f63805808
Nilachal sethi <nilachal01@gmail.com>
2021-09-26 07:40:28 +0530

    New Branch Setup

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -44,7 +44,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Neel Test WebAPI v1"));
             }
 
             app.UseRouting();
